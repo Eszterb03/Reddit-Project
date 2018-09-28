e18c7a172a6d0f34717169e08de4bfbe6de1d259
Eszterb03 <eszter.b03@gmail.com>
2018-09-28 12:26:06 +0200

    feat(down-,upvote) img is in the HTML

--- assets/index.js
+++ assets/index.js
@@ -11,14 +11,40 @@ window.onload = () => {
       posts.forEach(post => {
         const postDiv = document.createElement('div');
         postDiv.className = 'postDiv';
+        postParent.appendChild(postDiv);
+
+        const voter = document.createElement('div');
+        voter.className = 'voterDiv';
+
+        const upvote = document.createElement('div');
+        upvote.className = 'upvote';
+        const upvoteImg = document.createElement('img');
+        upvoteImg.setAttribute('src', 'assets/upvoted.png');
+        postDiv.appendChild(voter);
+        upvote.appendChild(upvoteImg);
+        voter.appendChild(upvote);
+
+
+        const downvote = document.createElement('div');
+        downvote.className = 'downvote';
+        const downvoteImg = document.createElement('img');
+        upvoteImg.setAttribute('src', 'assets/downvoted.png');
+        voter.appendChild(downvote);
+        downvote.appendChild(downvoteImg);
+
+
         const titlePost = document.createElement('h2');
         titlePost.innerText = post.title;
-        let postLink = document.createElement('a');
+        const postLinkDiv = document.createElement('div');
+        const postLink = document.createElement('a');
         postLink.setAttribute('href', post.url);
 
-        postDiv.appendChild(postLink);
+        postLinkDiv.appendChild(postLink);
+        postDiv.appendChild(postLinkDiv);
         postLink.appendChild(titlePost);
-        postParent.appendChild(postDiv);
+
+
+
       });
     }
   }
